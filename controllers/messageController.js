4cea430fb9141675f6d8d25b086c66c5f574a521
Alex N <xander.nesta@gmail.com>
2024-01-15 18:45:44 -0500

    implement delete button and functionality for admin users

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -2,10 +2,11 @@ const asyncHandler = require("express-async-handler")
 const { body, validationResult } = require("express-validator");
 const Message = require("../models/message")
 
+//Display New Message Form on GET
 exports.message_form_get = asyncHandler(async function(req, res, next) {
-    /* if (!req.user) {
+     if (!req.user) {
         res.redirect("/login");
-    } else { */
+    } else { 
         res.render("message_form", {
             title: "New Message",
             msgTitle: undefined,
@@ -13,22 +14,22 @@ exports.message_form_get = asyncHandler(async function(req, res, next) {
             errors: {},
             user: req.user,
         })
-    // }   
+    }   
 })
 
 exports.message_form_post = [
     body("title")
     .trim()
     .isString()
+    .withMessage("Only strings allowed in Title")
     .isLength({ min: 1 })
     .withMessage("Title is required"),
-    body("text")
+    body("msgText")
     .trim()
-    .isString()
     .isLength({ min: 1 })
     .withMessage("Message text is required")
-    .isLength({ max: 4 })
-    .withMessage("Message text cannot exceed 4 characters."),
+    .isLength({ max: 4000 })
+    .withMessage("Message text cannot exceed 4000 characters."),
 
     asyncHandler(async function(req, res, next) {
         if (!req.user) {
@@ -42,9 +43,10 @@ exports.message_form_post = [
 
         const message = new Message({
             title: req.body.title,
-            message: req.body.text,
-            author: req.user.id,
+            message: req.body.msgText,
+            author: req.user._id,
         });
+        console.dir('message: '+ message)
 
         if (!errors.isEmpty()){
             res.render("message_form", {
@@ -55,9 +57,44 @@ exports.message_form_post = [
                 user: req.user,
             })
         } else {
-            await message.save();
-
-            res.redirect("/")
+            try {
+                await message.save();
+                res.redirect("/");
+            } catch (err) {
+                console.log(err);
+                return next(err);
+            }
         }
 
-})]
+}),]
+
+//Display MESSAGE DELETE Confirmation page on GET
+exports.message_delete_get = asyncHandler(async (req, res, next) => {
+    const message = await Message.findById(req.params.id)
+    .populate("author")
+    .exec()
+
+    if(message === null){
+        //no results.
+        const err = new Error("Candle not found")
+        err.status = 404;
+        return next(err);
+    } else {
+        res.render("message_delete",{
+            title: "Delete Message Confirmation",
+            message: message,
+        })
+    }
+})
+
+//Handle MESSAGE DELETE on POST.
+exports.message_delete_post = asyncHandler(async (req, res, next) => {
+    if(req.user.isAdmin !== true){
+        const err = new Error("Unauthorized");
+        err.status = 401;
+        next(err);
+    } else {
+        await Message.findByIdAndDelete(req.body.messageid)
+        res.redirect('/');
+    }
+})
--- routes/message.js
+++ routes/message.js
@@ -6,7 +6,10 @@ const message_controller = require("../controllers/messageController")
 
 /* GET newMessage Page. */
 router.get('/', message_controller.message_form_get);
-/* GET newMessage Page. */
+/* POST newMessage Page. */
 router.post('/', message_controller.message_form_post);
-
+/* GET delete Message Page. */
+router.get('/:id/delete', message_controller.message_delete_get);
+/* POST delete Message Page. */
+router.post('/:id/delete', message_controller.message_delete_post);
 module.exports = router;
